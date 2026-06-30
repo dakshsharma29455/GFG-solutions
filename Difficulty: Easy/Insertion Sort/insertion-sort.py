91f89551fede81a8b79f7c3a63895b73359ce8f2
dakshsharma29455 <dakshsharma2945@gmail.com>
2026-06-30 21:35:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Insertion Sort/insertion-sort.py
@@ -0,0 +1,11 @@
+class Solution:
+    def insertionSort(self, arr):
+        n = len(arr)
+        for i in range(1, n):
+            j = i
+            while j>0 and arr[j-1]>arr[j]:
+                arr[j-1],arr[j] = arr[j],arr[j-1]
+                j -= 1
+        return arr        
+        # code here
+        

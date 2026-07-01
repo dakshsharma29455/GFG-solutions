5e605e86bcbfdb4037acfe675b70f05989fc474e
dakshsharma29455 <dakshsharma2945@gmail.com>
2026-07-01 20:56:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Insertion Sort/insertion-sort.py
+++ Difficulty: Easy/Insertion Sort/insertion-sort.py
@@ -4,7 +4,7 @@ class Solution:
         for i in range(1, n):
             j = i
             while j>0 and arr[j-1]>arr[j]:
-                arr[j-1],arr[j] = arr[j],arr[j-1]
+                arr[j-1],arr[j]=arr[j],arr[j-1]
                 j -= 1
         return arr        
         # code here

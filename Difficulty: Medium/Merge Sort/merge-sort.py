b75a7dfd2221e85b3db03b6a724435a6b3663f29
dakshsharma29455 <dakshsharma2945@gmail.com>
2026-07-01 20:35:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Merge Sort/merge-sort.py
@@ -0,0 +1,29 @@
+class Solution:
+    def mergeSort(self, arr, l, r):
+        if len(arr) > 1:
+            mid =len(arr) // 2
+            left = arr[:mid]
+            right = arr[mid:]
+            self.mergeSort(left, 0, len(left) - 1)
+            self.mergeSort(right, 0, len(right) - 1)
+            i = j = k = 0
+            while i < len(left) and j < len(right):
+                if left[i] < right[j]:
+                    arr[k] = left[i]
+                    i += 1
+                else:
+                    arr[k] = right[j]
+                    j += 1
+                k += 1
+            while i < len(left):
+                arr[k] = left[i]
+                i += 1
+                k += 1
+            while j < len(right):
+                arr[k] = right[j]
+                j += 1
+                k += 1    
+                
+        
+        # code here
+        

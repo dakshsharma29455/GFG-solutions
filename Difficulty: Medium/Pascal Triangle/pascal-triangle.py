33a2ec18850e79e89f8d82690d772130b8f1fb84
dakshsharma29455 <dakshsharma2945@gmail.com>
2026-07-12 22:53:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Pascal Triangle/pascal-triangle.py
@@ -0,0 +1,16 @@
+class Solution:
+    def nthRowOfPascalTriangle(self, n):
+        result = []
+
+        for i in range(n):
+            row = [1] * (i + 1)
+
+            for j in range(1, i):
+                row[j] = result[i - 1][j - 1] + result[i - 1][j]
+
+            result.append(row)
+
+        return result[-1]
+	    
+	    
+	    
